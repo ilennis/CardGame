b5fd10f0db589a182df609b9cfcfe6cc04a5236b
윤동영 <ehddud9608@naver.com>
2025-01-21 20:19:01 +0900

    Update Scene_End.cs

--- Assets/Scripts/SceneManager/Scene_End.cs
+++ Assets/Scripts/SceneManager/Scene_End.cs
@@ -1,5 +1,9 @@
+using UnityEngine.UI;
+
 public class Scene_End : Scene_Base
 {
+    public Text TryCount;
+    public Text attemptsCount;
     protected override void Initialize()
     {
         base.Initialize();
