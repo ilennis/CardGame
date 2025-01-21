0fbeebc770dde5063325a9235c803fcbf39e524d
hannah896 <phn2260@naver.com>
2025-01-21 16:02:10 +0900

    타이머, 배경 위치

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,32 @@
-public class GameManager
-{
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+public class GameManager : MonoBehaviour
+{
+    public Text timeTxt;
+
+    float time = 30.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+        
+        //시간이 다 됐을 때
+        if (time == 0)
+        {
+
+        }
+    }
 }
