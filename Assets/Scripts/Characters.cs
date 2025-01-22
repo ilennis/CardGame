bfaa718a8713c1eb43284876f2e6750873224bb6
윤동영 <ehddud9608@naver.com>
2025-01-22 22:18:05 +0900

    Stage Select구성

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class Characters : MonoBehaviour
+{
+    bool isLeft = false;
+    bool isRight = false;
+    float direction = 0.05f;
+    SpriteRenderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLeft)
+        { 
+            
+            
+            if (transform.position.x > -8.0f)
+            {
+                transform.position += Vector3.left * direction;
+                // transform.position += Vector3.left * 0f;
+            }
+        }
+        if(isRight)
+        {
+            
+            
+            if (transform.position.x < 7.6f)
+            {
+                transform.position += Vector3.right * direction;
+                //transform.position += Vector3.right * 0f;
+            }
+        }  
+    }
+    public void OnClick()
+    {
+        isLeft = true;
+        isRight = false;
+    }
+
+    public void reverseClick()
+    {
+        isRight = true;
+        isLeft = false;
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/StageButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageButton : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MakeEasyMode()
+    {
+        StageManager.Instance.SelectStage(StageType.Easy);
+    }
+    public void MakeHardMode()
+    {
+        StageManager.Instance.SelectStage(StageType.Hard);
+    }
+    
+
+
+}
